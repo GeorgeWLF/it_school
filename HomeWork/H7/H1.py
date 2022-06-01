8bd22c3533277e1e858b6380e4af04f2169476d0
George Wolf <georgewolfbiz@gmail.com>
2022-06-01 18:24:54 +0300

    update s12

---
+++ HomeWork/H7/H1.py
@@ -0,0 +1,10 @@
+# 1. Scrie o clasa care repr. o Agenda (Notebook).
+# Atribute:
+#     - nr pagini
+#     - culoare
+#     - continut - lista de stringuri ["continut pagina", "continut pag."]
+
+# Metode:
+#     - scrie in Agenda
+#     - vezi nr de pag goale
+#     - vezi nr pag scrise
